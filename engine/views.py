c629cc032fc637980835e78d0a2c75db270f10a5
mightycommander <mmardle@gmail.com>
2018-08-24 16:10:23 +0100

    Adding in the form

--- engine/views.py
+++ engine/views.py
@@ -2,6 +2,7 @@ from django.template import loader
 from django.http import HttpResponse
 from .models import ResortInfo, get_recommendations
 from django.shortcuts import render, get_object_or_404
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -11,7 +12,28 @@ def index(request):
     context = {
         'allResorts': allResorts,
     }
-    return render(request, 'engine/index.html', context)
+    page_title = 'An example form and graph'
+
+    form = ContactForm(request.form)
+
+    if request.method == 'GET':
+        return render(request, 'engine/index.html', context)
+
+    if request.method == 'POST':
+        flash("You just submitted a form. Yay!")
+        return render(request, 'engine/index.html', context)
+        '''
+        plot = create_dummy_plot(
+            project_id=request.form['project_id'],
+            plot_type=request.form['plot_type'],
+            color=request.form['plot_color']
+        )
+        script, div = components(plot)
+
+        return render_template(
+            'core/example.html', title=page_title, nav_links=nav_links, form=form, bokeh_script=script, div=div
+        )
+    '''
 
 
 
